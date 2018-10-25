f6f145f03b112ad15b75728180177c655ab3e0fa
陈金良 <jello_chen@live.com>
2018-10-25 23:03:44 +0800

    Add WebHostBuilderExtension class

--- MyTodo/Program.cs
+++ MyTodo/Program.cs
@@ -19,6 +19,7 @@ namespace MyTodo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseCmdUrl(args);
     }
 }
---
+++ MyTodo/WebHostBuilderExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace MyTodo
+{
+    public static class WebHostBuilderExtension
+    {
+        public static IWebHostBuilder UseCmdUrl(this IWebHostBuilder webHostBuilder, string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+            String ip = string.IsNullOrWhiteSpace(config["ip"]) ? "127.0.0.1" : config["ip"];
+            String port = string.IsNullOrWhiteSpace(config["port"]) ? "5000" : config["port"];
+            return webHostBuilder.UseUrls($"http://{ip}:{port}");
+        }
+    }
+}
